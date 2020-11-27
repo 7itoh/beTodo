e814983863861ebd19c3c30be70fc79067a6d9de
yosuke naito <yosukenaito@yosukenoMacBook-Pro.local>
2020-11-27 09:02:43 +0900

    imprement Todo develop Project 20201125 ver001

--- src/main.js
+++ src/main.js
@@ -1,91 +1,48 @@
 'use strict';
 
 /**
- * 
- * 新規Todo作成
- * createTodoInput 登録Todoの入力
+ * Todo 入力/出力
+ * @createTdInpt 登録Todoの入力
+ * @dispTdLists  TodoListsの一覧表示
  */ 
-const createTdInpt = document.getElementById('createTodoInput');
+const createTdInpt = document.getElementById('create_Todo_Input');
+const dispTdLists = document.getElementById('disp_Todo_Lists');
 
 /**
- * Todoリスト一覧　表示
+ * Radio入力
+ * @dispAllTskRadio   全件表示
+ * @dispMaintTskRadio メンテナンスタスクのみ表示
+ * @dispCompTskRadio  完了タスクのみ表示
  * 
- *　dispTodoLists TodoLists 表示
- */ 
-const dispTdLists = document.getElementById('dispTodoLists');
-
-/**
- * Radioボタン入力
- * 
- * dispAllTasksRadio       全件表示
- * dispMainteTasksRadio    メンテナンスタスクのみ表示
- * dispCompeleteTasksRadio 完了タスクのみ表示
- */ 
-
-const dispAllTskRadio = document.getElementById('dispAllTasksRadio');
-const dispMaintTskRadio = document.getElementById('dispMainteTasksRadio');
-const dispCompTskRadio = document.getElementById('dispCompeleteTasksRadio');
-
-/**
  * Button入力
- * 
- * createTodoButton Todoリスト新規作成
+ * @createTdBtn Todoリスト新規作成
  */ 
-const createTdBtn = document.getElementById('createTodoButton');
 
-/**
- * エラーメッセージ
- * 
- * errMesssage　エラーメッセージ表示
- */ 
-const errMsg = document.getElementById('errMesssage');
+const dispAllTskRadio = document.getElementById('disp_All_Tasks_Radio');
+const dispMaintTskRadio = document.getElementById('disp_Mainte_Tasks_Radio');
+const dispCompTskRadio = document.getElementById('disp_Compelete_Tasks_Radio');
+const createTdBtn = document.getElementById('create_Todo_Button');
 
 const todos = [];
 
-const createTdList = todo => { 
+const createTdList = todo => {
     const isValue = (element) => element === todo;
+    const id = todos.findIndex(isValue);
     console.log(todos.findIndex(isValue)); //indexチェック
-    console.log(todos.find(isValue));      //valueチェック
-
-    // タグ生成
-    const tdTableTr = document.createElement('tr');
-    const tdStatusTd = document.createElement('td');
-    const tdStatusBtn = document.createElement('button');
-    const tdDelTd = document.createElement('td');
-    const tdDelBtn = document.createElement('button');
-    tdStatusBtn.innerHTML = '作業中';
-    tdDelBtn.innerHTML = '削除';
-
-    const tdIdSpan = document.createElement('span');
-    tdIdSpan.innerHTML = todos.findIndex(isValue);
-    const tdIdSpanTd = document.createElement('td');
-    tdIdSpanTd.append(tdIdSpan);
-
-    const taskSpan = document.createElement('span');
-    const taskSpanTd = document.createElement('td');
-    taskSpan.innerText = todos.find(isValue);
-    taskSpanTd.append(taskSpan);
-    
-    tdStatusTd.appendChild(tdStatusBtn);
-    tdDelTd.appendChild(tdDelBtn);
-
-    tdTableTr.appendChild(tdIdSpanTd);
-    tdTableTr.appendChild(taskSpanTd);
-    tdTableTr.appendChild(tdStatusTd);
-    tdTableTr.appendChild(tdDelTd);
-
-    dispTdLists.appendChild(tdTableTr);
+    const tdElements ='<tr><td id="todo_List_Id_' + id + '">' + id + '</td><td id="todo_List_Task">' + todos[id].content + '</td><td><button id="todo_Status_Button_' + id + '">' + todos[id].state + '</button></td> <td><button id="todo_Delete_Button_' + id + '">' + todos[id].delete + '</button></td></tr > '
+    dispTdLists.insertAdjacentHTML("beforeend", tdElements);
 }
-
-// createTdBtn.addEventListener('click', createTdList);
 
 createTdBtn.addEventListener('click', () => {
     if (!createTdInpt.value) { 
-    errMsg.innerHTML = '「新規タスク追加」蘭に入力がありません'
     return;
     }
-    errMsg.innerHTML = '';
-    const todo = createTdInpt.value;
+    console.log(createTdInpt.value); //入力チェック
+    const todo = {
+        content: createTdInpt.value,
+        state: '実行中',
+        delete: '削除',
+    };
     todos.push(todo);
     createTdInpt.value = '';
     createTdList(todo);
